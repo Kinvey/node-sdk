1411a9b80d7de34500900bfc7f0a2de9b163b1d1
Thomas Conner <thomas.conner@me.com>
2017-11-17 10:29:39 -0500

    Delete files

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import Kinvey, { NetworkRack } from 'kinvey-js-sdk/dist/export';
-import { HttpMiddleware } from './middleware';
-
-// Setup racks
-NetworkRack.useHttpMiddleware(new HttpMiddleware());
-
-// Export
-module.exports = Kinvey;
